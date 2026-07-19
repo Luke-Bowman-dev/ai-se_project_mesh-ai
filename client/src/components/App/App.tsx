7a4909778d995934024d7beb1ba08fb67e45d825
Luke Bowman <huka7a@gmail.com>
2026-07-19 16:28:05 -0500

    Refactor: moved the App folder into the components folder

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css"
-import KnowledgeBase from "../pages/KnowledgeBase/KnowledgeBase";
+import KnowledgeBase from "../../pages/KnowledgeBase/KnowledgeBase";
 import { Routes, Route } from "react-router-dom";
-import AppLayout from "../components/AppLayout/AppLayout";
-import Intro from "../pages/Intro/Intro";
-import Chat from "../pages/Chat/Chat";
+import AppLayout from "../AppLayout/AppLayout";
+import Intro from "../../pages/Intro/Intro";
+import Chat from "../../pages/Chat/Chat";
 
 function App() {
  return (
